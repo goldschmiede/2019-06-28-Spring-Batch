25d71f58d76738ca4bb6f6e142b16204cba2d1ef
hjhessmann <hans.joerg.hessmann@anderscore.com>
2019-06-14 08:58:18 +0200

    RetryTemplate

--- samples/src/test/java/com/anderscore/goldschmiede/springbatch/samples/exec/RetryTemplateTest.java
+++ samples/src/test/java/com/anderscore/goldschmiede/springbatch/samples/exec/RetryTemplateTest.java
@@ -5,20 +5,25 @@ import java.util.Collections;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.retry.RetryContext;
+import org.springframework.retry.backoff.ExponentialBackOffPolicy;
 import org.springframework.retry.policy.SimpleRetryPolicy;
 import org.springframework.retry.support.RetryTemplate;
 
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 public class RetryTemplateTest {
 
     @Test
     public void test() throws FileNotFoundException {
         RetryTemplate retryTemplate = new RetryTemplate();
-        retryTemplate.setRetryPolicy(new SimpleRetryPolicy(3, Collections.singletonMap(FileNotFoundException.class, true)));
+        retryTemplate.setRetryPolicy(new SimpleRetryPolicy(10, Collections.singletonMap(FileNotFoundException.class, true)));
+        retryTemplate.setBackOffPolicy(new ExponentialBackOffPolicy());
         Integer execute = retryTemplate.execute(this::action);
     }
 
     private Integer action(RetryContext retryContext) throws FileNotFoundException {
-        System.out.println("action");
+        log.info("action");
         throw new FileNotFoundException("test.txt");
     }
 }
