23db61900c59c737163f8e3a43e5e56002e72a9d
hjhessmann <hans.joerg.hessmann@anderscore.com>
2019-06-15 17:53:17 +0200

    Simple parallel execution

---
+++ samples/src/test/java/com/anderscore/goldschmiede/springbatch/samples/parallel/ParallelStepTest.java
@@ -0,0 +1,26 @@
+package com.anderscore.goldschmiede.springbatch.samples.parallel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import com.anderscore.goldschmiede.springbatch.samples.parallel.ParallelStepConfig;
+
+@SpringJUnitConfig(ParallelStepConfig.class)
+public class ParallelStepTest {
+
+    @Autowired
+    private JobLauncher jobLauncher;
+
+    @Autowired
+    private Job job;
+
+    @Test
+    void testRetry() throws Exception {
+        jobLauncher.run(job, new JobParameters());
+    }
+
+}
